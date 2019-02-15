c101e3eb7fd234ea9b354d0e4f923d8e96228335
wojtek <kefiras@gmail.com>
2019-02-15 13:26:42 -0800

    Add network stuff

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ export ELASTICSEARCH_USERNAME=elastic
 export ELASTICSEARCH_PASSWORD=changeme
 export ELASTIC_VERSION=6.6.0
 export DOMAIN=home.web-sec.net
-
+docker network rm elastic
+docker network create -d overlay elastic
 envsubst < logstash.conf.default > logstash.conf
 docker stack deploy -c docker-compose.yml elastic
