fd9efb2ccedd6ef957df52b84ce3c0b0c732b2f9
wojtas <kefiras@gmail.com>
2019-02-15 19:50:58 +0100

    Add -c

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ export ELASTICSEARCH_USERNAME=elastic
 export ELASTICSEARCH_PASSWORD=changeme
 export ELASTIC_VERSION=6.6.0
 
-docker stack deploy docker-compose.yml elastic
+docker stack deploy -c docker-compose.yml elastic
