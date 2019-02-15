365105cf07fa2e5b73ba339bfe9a4db67dc493f9
wojtek <kefiras@gmail.com>
2019-02-15 12:30:36 -0800

    Add domain variable

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 export ELASTICSEARCH_USERNAME=elastic
 export ELASTICSEARCH_PASSWORD=changeme
 export ELASTIC_VERSION=6.6.0
+export DOMAIN=home.web-sec.net
 
 envsubst < logstash.conf.default > logstash.conf
 docker stack deploy -c docker-compose.yml elastic
